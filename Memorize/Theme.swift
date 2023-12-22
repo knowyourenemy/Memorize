27cbe613ff49d1aa008c55f5497196bc64f937ab
Aaditya Patwari <aadipatwari@gmail.com>
2023-12-22 19:49:08 +0800

    Add theme editor view.

--- Memorize/Theme.swift
+++ Memorize/Theme.swift
@@ -10,8 +10,8 @@ import SwiftUI
 struct Theme: Hashable, Identifiable {
     var id: String { name }
     
-    let name: String;
-    let emojis: Array<String>;
-    let numberOfPairs: Int;
-    let color: String;
+    var name: String;
+    var emojis: Array<String>;
+    var numberOfPairs: Int;
+    var color: String;
 }
---
+++ Memorize/ThemeEditor.swift
@@ -0,0 +1,54 @@
+//
+//  ThemeEditor.swift
+//  Memorize
+//
+//  Created by Aaditya Patwari on 22/12/23.
+//
+
+import SwiftUI
+
+struct ThemeEditor: View {
+    @Binding var theme: Theme
+    @State var emojis: String
+    
+    init(theme: Binding<Theme>) {
+        self._theme = theme
+        emojis = theme.emojis.wrappedValue.joined()
+        
+    }
+    
+    
+    var body: some View {
+        Form {
+            Section(header: Text("Name")) {
+                TextField("Name", text: $theme.name)
+            }
+            Section(header: Text("Emojis")) {
+                TextField("Emojis", text: $emojis).onChange(of: emojis) {
+                    theme.emojis = emojis.split(separator: "").map(String.init)
+                }
+            }
+            Section(header: Text("Details")) {
+                Picker("No. of pairs", selection: $theme.numberOfPairs) {
+                    ForEach(2...theme.emojis.count, id: \.self) { numberOfPairs in
+                        Text(numberOfPairs, format: .number)
+                        
+                    }
+                }
+                .pickerStyle(.menu)
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    struct Preview: View {
+        @State var theme: Theme = ThemeStore.builtins.first!
+        
+        var body: some View {
+            ThemeEditor(theme: $theme)
+        }
+    }
+    return Preview()
+}
